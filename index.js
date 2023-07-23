57fa5553202cb792114129d5f78e25bdc5046885
Sushil Sharma <sushilkumar.here@gmail.com>
2023-07-23 21:11:48 +0530

    11

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var colors = require('colors');
- 
+const express =require('express');
+const path = require('path');
 
-const http= require('http');
-const data = require('./data');
-http.createServer((req,resp)=>{
-resp.writeHead(500,{'Content-Type':'application\json'});
-resp.write(JSON.stringify(data));
-resp.end();
-}).listen(5000);
+
+const app = express();
+const publicpath = path.join(__dirname,'public');
+app.use(express.static(publicpath));
+app.set('view engine','ejs');
+app.get('',(req,resp)=>{
+    resp.sendFile('${publicpath}/index.html')
+})
+
+app.get('/about',(req,resp)=>{
+    resp.sendFile('${publicpath}/about.html')
+})
+app.get('/profile',(req,resp)=>{
+    const user = { name:'Sushil sharma',email : 'sushilkumar.here@gmail.com'
+
+    }
+    resp.render('profile',{user});
+})
+
+app.listen(5000);
