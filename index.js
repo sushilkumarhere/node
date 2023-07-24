e2472f728b879d857b96bacbfd54201fe73eaab1
Sushil Sharma <sushilkumar.here@gmail.com>
2023-07-24 10:04:18 +0530

    240723

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const app = express();
 const publicpath = path.join(__dirname,'public');
 app.use(express.static(publicpath));
 app.set('view engine','ejs');
+const reqfilter = (req,resp,next)=>{
+    //console.log('reqFilter');
+    if(!req.query.age)
+    {
+        resp.send("please provide your age first::")
+    }
+    else if(req.query.age < 10)
+    {
+        resp.send("You can not access this site:")
+    }
+    else{
+        next();
+
+    }
+   
+}
+
+app.use(reqfilter)
+
 app.get('',(req,resp)=>{
     resp.sendFile('${publicpath}/index.html')
 })
